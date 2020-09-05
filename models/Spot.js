5449de43f14b92aab3f1950a0ee9a83dd7b54db6
unknown <ola.anders.karlsson@gmail.com>
2020-09-05 21:40:32 +0200

    ...

--- models/Spot.js
+++ models/Spot.js
@@ -3,7 +3,23 @@ const mongoose = require('mongoose');
 const spotSchema = new mongoose.Schema({
   name: String,
   lon: String,
-  lat: String
+  lat: String,
+  windDirections: {
+    N:String,
+    NO:String,
+    NV:String,
+    O:String,
+    V:String,
+    SO:String,
+    SV:String,
+    S:String
+  },
+  reviews:{
+    launchFriendly: Number,
+    shallow: Number,
+    waves: Number,
+    accesibility: Number},
+  description: String
 }, { timestamps: true });
 
 const Spot = mongoose.model('Spot', spotSchema);
