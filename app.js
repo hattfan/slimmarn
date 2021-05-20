993e350ec8030198a1749da8ba0547a1721f652c
unknown <ola.anders.karlsson@gmail.com>
2021-05-20 21:08:32 +0200

    pushtest

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(sass({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public')
 }));
+// funkar ej
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
